e2cd9416292a4a57e0ab0ece617dbc4ad488ec94
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-09-09 22:56:24 -0600

    se agrego nuevo proyecto

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,4 +1,23 @@
-// import 'mocha'
-// import { expect, should } from 'chai'
-// import mongoose from 'mongoose'
-// import Activity from '../models/activity'
+import 'mocha'
+import { expect, should } from 'chai'
+import mongoose from 'mongoose'
+import Payment from '../models/payment'
+
+
+describe('Payments',()=>{
+  describe('create',()=>{
+    it('Should create a payment')
+    it('')
+    it('')
+  })
+  describe('delete',()=>{
+    it('')
+    it('')
+    it('')
+  })
+  describe('update',()=>{
+    it('')
+    it('')
+    it('')
+  })
+})
