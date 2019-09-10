5e9b7280b6f72b243bf52ba8f609c9d273182686
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-09-09 23:01:05 -0600

    Se continua

--- test/payment.test.js
+++ test/payment.test.js
@@ -7,9 +7,8 @@ import Payment from '../models/payment'
 describe('Payments',()=>{
   describe('create',()=>{
     it('Should create a payment',()=>{
-      const payment = new Payment({'user':'123123','amount':'123.123','description':'Pago de casona'})
-      payment.save(done)
-      console.log('pagado')
+      const payment = new Payment({user:'123123',amount:'123.123',description:'Pago de casona'})
+      payment.save()
     })
     it('')
     it('')
