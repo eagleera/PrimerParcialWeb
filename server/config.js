0b614b64acdf92bbc5db554aacf87f5c8cdd7a1f
Alfonso Villalobos <alfonsocvu@gmail.com>
2019-09-09 20:52:56 -0600

    change port

--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 export const port = process.env.PORT || 3000
-export const connexionString = 'mongodb://localhost/test'
+export const connexionString = 'mongodb://localhost:27017/test'
 export const baseApi = 'api'
